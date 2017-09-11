456bfca57b7734ea8be0c41cf992a3f7d623015e
Bradly Swart <bswart@atlassian.com>
2017-09-11 16:55:26 +1000

    route and page for adding categories

---
+++ routes/categories.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+var mongo = require('mongodb')
+var db = require('monk')('localhost/nodeblog')
+
+/* GET posts */
+router.get('/add', function(req, res, next) {
+  res.render('categories/add', {title: "Add Category"})
+});
+
+router.post('/add', function(req, res, next) {
+  var name = req.body.name
+  // Form field validation
+  req.checkBody('name', 'Enter a category name.').notEmpty()
+
+  var errors = req.validationErrors()
+  if(errors){
+    res.render('categories/add', {
+      "errors": errors
+    })
+  }else{
+    var posts = db.get('categories')
+    posts.insert({
+      'name': name
+    }, function(err, post){
+      if(err){
+        res.send(err)
+      }else{
+        req.flash('success', 'Post added')
+        res.location('/')
+        res.redirect('/')
+      }
+
+    })
+  }
+});
+
+module.exports = router;
